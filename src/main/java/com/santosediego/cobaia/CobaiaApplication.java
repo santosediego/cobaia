5adf16dd4a40f5156d0f9ce9cc7b306c862c384e
santosediego <diegosantos.dev@outlook.com>
2023-07-13 19:28:54 -0300

    FEAT: Project created

---
+++ src/main/java/com/santosediego/cobaia/CobaiaApplication.java
@@ -0,0 +1,13 @@
+package com.santosediego.cobaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobaiaApplication.class, args);
+	}
+
+}
