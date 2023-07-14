e3faef24a2c7b4681364fbd87cf70972e4e7aec1
santosediego <diegosantos.dev@outlook.com>
2023-07-14 19:01:01 -0300

    FEAT: SEED created

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO TB_PERSON (NAME, CPF, EMAIL, PHONE) VALUES ('José Maria', '36238916060', 'jose.maria@email.com', '34983579114');
+INSERT INTO TB_PERSON (NAME, CPF, EMAIL, PHONE) VALUES ('Maria José', '46674793612', 'maria.jose@email.com', '34995783326');
+INSERT INTO TB_PERSON (NAME, CPF, EMAIL, PHONE) VALUES ('Alícia Caroline Moraes', '18479457678', 'alicia.morais@email.com', '34992926504');
+INSERT INTO TB_PERSON (NAME, CPF, EMAIL, PHONE) VALUES ('Noah Lucas Cavalcanti', '26275183640', 'noah.cavalcanti@email.com', '34985413724');
+INSERT INTO TB_PERSON (NAME, CPF, EMAIL, PHONE) VALUES ('Clara Porto', '58428096643', 'clara.porto@email.com', '34985945321');
+
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Rua das Petúnias', '753', 'Cidade Jardim', '38412112', 'Uberlândia', 1);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Avenida Estela Saraiva de Peano', '654', 'Jardim Patrícia', '38414094', 'Uberlândia', 1);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Rua Paraíba', '122', 'Marta Helena', '38402037', 'Uberlândia', 2);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Rua Genuveva Rezende dos Santos', '312', 'Jardim Ipanema', '38406259', 'Uberlândia', 3);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Rua Serra Santa Marta', '681', 'São Jorge', '38410369', 'Uberlândia', 4);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Rua Machado de Assis', '378', 'Lídice', '38400081', 'Uberlândia', 5);
+INSERT INTO TB_ADDRESS (ROAD, NUMBER, DISTRICT, ZIP_CODE, CITY, PERSON_ID) VALUES ('Avenida Floriano Peixoto', '778', 'Custódio Pereira', '38405184', 'Uberlândia', 5);
